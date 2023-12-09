69ffc1e6ca8ec6d31e97df5679978f0d6f23281f
jgryzen <jgryzen@OISIT-IST37604.unomaha.edu>
2023-12-09 14:54:52 -0600

    preds

--- multiple_run_small_models.py
+++ multiple_run_small_models.py
@@ -68,13 +68,13 @@ os.system('python predict.py --m=facebook/opt-2.7b --t=qa1_single-supporting-fac
 # Two Supporting Facts 
 
 # No Context
-os.system('python predict.py --m=facebook/opt-350m --t=qa2_two-supporting-facts_train.txt.txt')
-os.system('python predict.py --m=facebook/opt-1.3b --t=qa2_two-supporting-facts_train.txt.txt')
-os.system('python predict.py --m=facebook/opt-2.7b --t=qa2_two-supporting-facts_train.txt.txt')
+os.system('python predict.py --m=facebook/opt-350m --t=qa2_two-supporting-facts_train.txt')
+os.system('python predict.py --m=facebook/opt-1.3b --t=qa2_two-supporting-facts_train.txt')
+os.system('python predict.py --m=facebook/opt-2.7b --t=qa2_two-supporting-facts_train.txt')
 # With Context
-os.system('python predict.py --m=facebook/opt-350m --t=qa2_two-supporting-facts_train.txt.txt --context')
-os.system('python predict.py --m=facebook/opt-1.3b --t=qa2_two-supporting-facts_train.txt.txt --context')
-os.system('python predict.py --m=facebook/opt-2.7b --t=qa2_two-supporting-facts_train.txt.txt --context')
+os.system('python predict.py --m=facebook/opt-350m --t=qa2_two-supporting-facts_train.txt --context')
+os.system('python predict.py --m=facebook/opt-1.3b --t=qa2_two-supporting-facts_train.txt --context')
+os.system('python predict.py --m=facebook/opt-2.7b --t=qa2_two-supporting-facts_train.txt --context')
 
 
 # endregion
